4b5489d33cb08d7286b1288ed0c9cd2d849e7338
Dan Vanderkam <danvdk@gmail.com>
2014-09-06 18:26:19 -0400

    Working player rankings API

---
+++ api.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+# Bits of information I need:
+# - Player rankings
+# - How each player did in last year's tournament
+# - What the draw looks like this year
+
+import re
+
+import requests
+import requests_cache
+from bs4 import BeautifulSoup
+
+requests_cache.install_cache('.cache')
+
+EU_RANKINGS = 'http://live-tennis.eu/official_atp_ranking'
+
+
+class DoubleAddExeption(Exception):
+    pass
+
+
+class PlayerPool(object):
+    def __init__(self):
+        self._players = []
+
+    def getPlayer(self, playerName):
+        candidates = [p for p in self._players if p.name == playerName]
+        if len(candidates) == 0:
+            raise KeyError('No player named %s in cache.' % playerName)
+        return candidates[0]
+
+    def addPlayer(self, player):
+        try:
+            p = self.getPlayer(player.name)
+        except KeyError:
+            self._players.append(player)
+        else:
+            raise DoubleAddException('Tried to add %s twice' % player.name)
+
+    def getOrAdd(self, name, nationality):
+        try:
+            return self.getPlayer(name)
+        except KeyError as e:
+            p = Player(name, nationality)
+            self.addPlayer(p)
+            return p
+
+
+_player_pool = PlayerPool()
+
+
+class Player(object):
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+
+
+class RankedPlayer(object):
+    def __init__(self, player, rank, points):
+        self.player = player
+        self.rank = rank
+        self.points = points
+
+
+class Rankings(object):
+    def __init__(self):
+        r = requests.get(EU_RANKINGS)
+        self._rankings = []
+        self._parseRankings(r.content)
+
+    def getPlayerAtRank(self, rank):
+        return self._rankings[rank]
+
+    def rankingForPlayer(self, playerName):
+        p = _player_pool.getPlayer(playerName)
+        ps = [r for r in self._rankings if r.player == p]
+        return ps[0]
+
+    def _parseRankings(self, html):
+        bs = BeautifulSoup(html)
+        rows = bs.select('#playerRankings table table tr')
+        for row in rows:
+            # cell1 is the ranking, e.g. 1, 2, 3
+            cells = row.select('td')
+            cell1 = cells[0].text.strip()
+            if not re.match(r'^[0-9]+$', cell1): continue
+
+            rank = int(cell1)
+            name = cells[2].text
+            nationality = cells[3].text
+            points = int(cells[4].text)
+            p = _player_pool.getOrAdd(name, nationality)
+            self._rankings.append(RankedPlayer(p, rank, points))
+
+
+if __name__ == '__main__':
+    rankings = Rankings()
